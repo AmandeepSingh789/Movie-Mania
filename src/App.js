c7dca00fa4603e99c0cd3875662596655045adc1
Amandeep <singh.amandeep5665@gmail.com>
2022-04-28 17:02:54 -0700

    fixed load problem

--- src/App.js
+++ src/App.js
@@ -5,28 +5,22 @@ import "./App.css";
 import SearchIcon from "./search.svg";
 // 85c97aa2
 // http://www.omdbapi.com/?i=tt3896198&apikey=85c97aa2
+let searches =0;
+function incrementCounter(){
+    searches=searches+1;
+}
 const api_URL = "http://www.omdbapi.com/?apikey=85c97aa2";
-
-const movie1 = {
-  Title: "Spider-Man: Homecoming",
-  Year: "2017",
-  imdbID: "tt2250912",
-  Type: "movie",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BNTk4ODQ1MzgzNl5BMl5BanBnXkFtZTgwMTMyMzM4MTI@._V1_SX300.jpg",
-};
 const App = () => {
+
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
   const searchMovies = async (title) => {
     const response = await fetch(`${api_URL}&s=${title}`);
     const data = await response.json();
     setMovies(data.Search);
+    searches=searches+1;
   };
 
-  useEffect(() => {
-    searchMovies("The Avengers");
-  }, []);
   return (
     <div className="app">
       <h1>MovieMania</h1>
@@ -40,7 +34,8 @@ const App = () => {
 
         <img src={SearchIcon} 
         alt="search" 
-        onClick={() => searchMovies(searchTerm)} />
+        onClick={() => searchMovies(searchTerm)}
+        />
 
 
       </div>
@@ -52,7 +47,13 @@ const App = () => {
         </div>
       ) : (
         <div className="empty">
-          <h2> No movies found</h2>
+            {searches > 0 ?
+            (<h2>Movie Not Found</h2>)
+            : (
+                <h2>Search Your Favorite Movie </h2>
+            )
+         }
+          
         </div>
       )}
     </div>
